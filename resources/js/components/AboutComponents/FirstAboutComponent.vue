61f2e4d5c9964cc154aa266db0142263878081bc
kaneki13383 <danchik.kun@mail.ru>
2022-12-18 02:19:55 +0400

    Страница о нас готова

--- resources/js/components/AboutComponents/FirstAboutComponent.vue
+++ resources/js/components/AboutComponents/FirstAboutComponent.vue
@@ -4,14 +4,29 @@
             <p class="pagination">Главная / Профиль / О нас</p>
             <h2>О нас</h2>
         </div>
-        <div class="d-f_why_we">
-            <div class="why_we">
-                <h3>Почему именно мы?</h3>
-                <p>Потому, что у нас только свежие продукты, которые привозят каждый день. Все блюда, приготовленные нашими поварами, получили максимальныые оценки критиков! Наше кафе работает уже на протяжении 10 лет, люди посетившие наше заведение остаются довольными и сытыми. ТЕСНОТА - выбор гурманов!</p>
+        <div class="d-f_info">            
+            <div class="d-f_why_we">
+                <div class="why_we">
+                    <h3>Почему именно мы?</h3>
+                    <p>Потому, что у нас только свежие продукты, которые привозят каждый день. Все блюда, приготовленные нашими поварами, получили максимальныые оценки критиков! Наше кафе работает уже на протяжении 10 лет, люди посетившие наше заведение остаются довольными и сытыми. ТЕСНОТА - выбор гурманов!</p>
+                </div>
+                <div class="why_we">
+                    <h3>Наши особенности</h3>
+                    <p>Наш шеф-повар получил премию "Лучший шеф-повар года". Зерна для нашего кофе поставляются исключительно из Колубии. Блюда приготовленные в нашем кафе имеют доступную цену. В честь праздников мы делаем притные бонусы и скидки.</p>
+                </div>
+                <router-link to="">Перейти к меню <p class="arrow">→</p></router-link>
             </div>
-            <div class="why_we">
-                <h3>Наши особенности</h3>
-                <p>Наш шеф-повар получил премию "Лучший шеф-повар года". Зерна для нашего кофе поставляются исключительно из Колубии. Блюда приготовленные в нашем кафе имеют доступную цену. В честь праздников мы делаем притные бонусы и скидки.</p>
+            <div class="img_text">
+                <div class="background_img_text">
+                    <p>“</p>
+                    <p>Идейные соображения высшего порядка, а также семантический разбор внешних противодействий однозначно фиксирует необходимость системы массового участия.  </p>
+                    <span>Юрий Тишков</span>
+                </div>
+                <div class="background2_img_text background_img_text">
+                    <p>“</p>
+                    <p>Идейные соображения высшего порядка, а также семантический разбор внешних противодействий однозначно фиксирует необходимость системы массового участия.  </p>
+                    <span>Юрий Тишков</span>
+                </div>
             </div>
         </div>
     </div>
@@ -24,6 +39,16 @@ export default {
 
         };
     },
+
+    mounted(){
+        document.title = 'О нас';
+        document.querySelector('.d-f_why_we a').addEventListener('mouseover', function(){
+            document.querySelector('.arrow').style.color = "#212529";
+        })
+        document.querySelector('.d-f_why_we a').addEventListener('mouseleave', function(){
+            document.querySelector('.arrow').style.color = "#af3131";
+        })
+    }
 };
 </script>
 
@@ -48,7 +73,6 @@ export default {
     }
     .why_we{
         color: white;
-        margin-top: 75px;
     }
     .why_we h3{
         font-size: 32px;
@@ -57,12 +81,77 @@ export default {
     }
     .why_we p{
         font-size: 20px;
-        width: 840px;
+        width: 800px;
         font-family: 'Roboto', sans-serif;
     }
     .d-f_why_we{
         display: flex;
         flex-direction: column;
         gap: 90px;
+    }
+    .d-f_why_we a{
+        font-family: "Comfortaa", serif;
+        text-decoration: none;
+        font-size: 20px;
+        color: white;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        border: 2px #af3131 solid;
+        border-radius: 15px;
+        width: 305px;
+        height: 41px;
+        padding: 10px 25px;
+        transition: .5s;
+    }
+    .d-f_why_we a:hover{
+        background: #af3131;
+        color: #212529;
+        font-weight: bold;
+    }
+    .arrow{
+        font-size: 50px;
+        margin-left: 70px;
+        color: #af3131;
+        transition: .5s;
+    }
+    .img_text{
+        display: flex;
+        flex-direction: column;
+        gap: 90px;
+    }
+    .background_img_text{
+        width: 685px;
+        height: 215px;
+        background-image: url('img/about_img.png');
+        background-repeat: no-repeat;
+        background-size: cover;
+        border-radius: 15px;
+        font-family: 'Cabin', sans-serif;
+        font-size: 20px;
+        color: white;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        padding: 45px;
+    }
+    .background_img_text p:first-child{
+        font-size: 128px;
+        height: 50px;
+        margin-top: -30px;
+    }
+    .background_img_text p:nth-child(2){
+        font-family: "Comfortaa", serif;
+    }
+    span{
+        font-weight: bold;
+    }
+    .background2_img_text{
+        background-image: url('img/about_img2.png');
+    }
+    .d-f_info{
+        display: flex;
+        justify-content: space-evenly;
+        margin: 75px 0;
     }
 </style>
