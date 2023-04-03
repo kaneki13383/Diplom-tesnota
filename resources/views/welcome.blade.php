b75d00bef69892787fe082076b0a349147647181
Allogikal <kimag200324@bk.ru>
2023-04-03 14:28:56 +0400

    Немного стилей для админки

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,32 +1,41 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel</title>
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-        <style>
-            *{
-                margin: 0;
-                padding: 0;
-                list-style-type: none;
-            }
-            a{
-                text-decoration: none
-            }
-            body{
-                background: #212529;
-            }
-            input, textarea{
-                outline: none;
-            }
-            html{
-                overflow-x: hidden;
-            }
-        </style>
-    </head>
-    <body id="app">     
 
-    
-    </body>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Laravel</title>
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
+  <style>
+    * {
+      margin: 0;
+      padding: 0;
+      list-style-type: none;
+    }
+
+    a {
+      text-decoration: none
+    }
+
+    body {
+      background: #212529;
+    }
+
+    input,
+    textarea {
+      outline: none;
+    }
+
+    html {
+      overflow-x: hidden;
+    }
+  </style>
+</head>
+
+<body id="app">
+
+  <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</body>
+
 </html>
